c8e9014fcd920da047bb56a64ebeb5a076dfdb9d
Dambue <r.dambuev@gmail.com>
2021-02-24 18:56:42 +0800

    ver. 1.1

--- src/main/java/com/dambue/spring_boot/service/UserServiceImpl.java
+++ src/main/java/com/dambue/spring_boot/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.dambue.spring_boot.model.Role;
 import com.dambue.spring_boot.model.User;
 import com.dambue.spring_boot.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,23 +18,33 @@ public class UserServiceImpl implements UserService {
         this.userRepository = userRepository;
     }
 
+    @Override
+    @Transactional
     public List<User> index() {
         return userRepository.findAll();
     }
 
+    @Override
+    @Transactional
     public void save(User user) {
         user.setRoles(Collections.singleton(new Role("ROLE_USER")));
         userRepository.save(user);
     }
 
+    @Override
+    @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
 
+    @Override
+    @Transactional
     public User show(Long id) {
         return userRepository.getOne(id);
     }
 
+    @Override
+    @Transactional
     public void update(Long id, User updUser) {
         userRepository.getOne(id);
         userRepository.save(updUser);
